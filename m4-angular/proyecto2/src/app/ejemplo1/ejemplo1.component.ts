8388305686da777a7ce7aeb8c0e20b040736f2d9
ExelObregon <edsselobregon@gmail.com>
2023-05-03 11:57:29 +0200

    proyecto2

--- m4-angular/proyecto2/src/app/ejemplo1/ejemplo1.component.ts
+++ m4-angular/proyecto2/src/app/ejemplo1/ejemplo1.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+
+interface Iproduct {
+  id: number;
+  title: string;
+  price: number;
+}
+
+
 @Component({
   selector: 'app-ejemplo1',
   templateUrl: './ejemplo1.component.html',
@@ -7,6 +15,17 @@ import { Component } from '@angular/core';
 })
 export class Ejemplo1Component {
 
+  // variables 
   mensaje:string = 'hola mundo desde ejemplo1'
   precio: number = 3.99;
+
+
+// objeto
+  product1: Iproduct = {
+    id: 1,
+    title: 'ordenador portatil',
+    price: 1500.0
+  }
+
+
 }
